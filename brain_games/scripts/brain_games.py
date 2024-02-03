477ce94f330178bcbc30def87ba68ed41083589c
Romanavr <rosin2134@gmail.com>
2024-02-03 11:09:28 +0300

    Fix lints

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,12 +2,15 @@
 
 from brain_games import cli
 
+
 def greetings():
     print('Welcome to the Brain Games!')
+
 
 def main():
     greetings()
     cli.welcome_user()
+
 
 if __name__ == '__main__':
     main()
