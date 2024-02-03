c6fbf141b392b8e166c0df7639d07a74aa5c0970
Romanavr <rosin2134@gmail.com>
2024-02-03 11:02:12 +0300

    1.3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+from brain_games import cli
+
 def greetings():
     print('Welcome to the Brain Games!')
 
 def main():
     greetings()
+    cli.welcome_user()
 
 if __name__ == '__main__':
     main()
