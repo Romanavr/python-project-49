81e445a7e9c4fbb9dc43d5b4bce2a8a29c6dbc32
Romanavr <rosin2134@gmail.com>
2024-02-08 00:47:00 +0300

    Fix prime number game

--- brain_games/games/prime_number.py
+++ brain_games/games/prime_number.py
@@ -4,7 +4,9 @@ GAME_DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no"
 
 
 def is_number_prime(number):
-    if number <= 1 or number % 2 == 0:
+    if number == 2:
+        return True
+    elif number <= 1 or number % 2 == 0:
         return False
     for i in range(3, number, 2):
         if number % i == 0:
