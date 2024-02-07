dca664f570e9ac9ebe4ede9c6281017877112017
Romanavr <rosin2134@gmail.com>
2024-02-07 22:43:00 +0300

    Fix prime number

--- brain_games/games/prime_number.py
+++ brain_games/games/prime_number.py
@@ -1,21 +1,17 @@
 from random import randrange
 
-GAME_DESCRIPTION = '''
-Answer "yes" if given number is prime.
-Otherwise answer "no".
-'''
+GAME_DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_number_prime(number):
     if number <= 1 or number % 2 == 0:
-        return number, 'no'
+        return False
     for i in range(3, number, 2):
-        print(i)
         if number % i == 0:
-            return number, 'no'
-    return number, 'yes'
+            return False
+    return True
 
 
 def game_handler():
     number = randrange(1, 101)
-    return number, 'yes' if is_number_prime(number) else 'no'
+    return number, ('yes' if is_number_prime(number) else 'no')
